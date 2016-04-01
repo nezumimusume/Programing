11da2390d0f9afd11649645127d591fbb8a0c6d8
KawaharaKiyohara <nezumimusume@gmail.com>
2016-04-01 13:25:05 +0900

    レンダリングターゲットの作成テストを追加。

---
+++ Packman/Packman/Test/TestCreateRenderTarget.cpp
@@ -0,0 +1,18 @@
+/*!
+ *@brief	レンダリングターゲットの作成テスト。
+ */
+
+#include "stdafx.h"
+#include "test/TestCreateRenderTarget.h"
+
+void CTestCreateRenderTarget::Start()
+{
+}
+void CTestCreateRenderTarget::Update()
+{
+	m_renderTarget.Create(
+	);
+}
+void CTestCreateRenderTarget::Render(tkEngine::CRenderContext& renderContext)
+{
+}
