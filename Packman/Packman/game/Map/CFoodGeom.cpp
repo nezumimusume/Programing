893ba25b795f6c2e2aefa456e7ebcba1afeaf776
KawaharaKiyohara <nezumimusume@gmail.com>
2016-04-12 09:47:43 +0900

    食べ物の形状クラスを作成。

---
+++ Packman/Packman/game/Map/CFoodGeom.cpp
@@ -0,0 +1,26 @@
+/*!
+ *@brief	食べ物の形状クラス。
+ */
+
+#include "stdafx.h"
+#include "Packman/game/map/CFoodGeom.h"
+
+/*!
+ *@brief	コンストラクタ。
+ */
+CFoodGeom::CFoodGeom()
+{
+}
+/*!
+ *@brief	デストラクタ。
+ */
+CFoodGeom::~CFoodGeom()
+{
+}
+/*!
+ *@brief	構築。
+ *@param[in]	radius		半径
+ */
+void CFoodGeom::Build( f32 radius )
+{
+}
---
+++ Packman/Packman/game/Map/CFoodGeom.h
@@ -0,0 +1,28 @@
+/*!
+ *@brief	食べ物の形状クラス。
+ */
+
+#ifndef _CFOODGEOM_H_
+#define _CFOODGEOM_H_
+
+#include "tkEngine/shape/tkSphereShape.h"
+
+class CFoodGeom{
+	/*!
+	 *@brief	コンストラクタ。
+	 */
+	CFoodGeom();
+	/*!
+	 *@brief	デストラクタ。
+	 */
+	~CFoodGeom();
+	/*!
+	 *@brief	構築。
+	 *@param[in]	radius		半径
+	 */
+	void Build( f32 radius );
+private:
+	tkEngine::CSphereShape	m_sphere;
+};
+
+#endif //_CFOODGEOM_H_
