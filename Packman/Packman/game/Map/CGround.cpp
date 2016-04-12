7eac15fe6ab2aec9d6736e1405de8f05d07836f0
KawaharaKiyohara <nezumimusume@gmail.com>
2016-04-12 14:53:19 +0900

    地面に影が落ちなくなっていたので修正。

--- Packman/Packman/game/Map/CGround.cpp
+++ Packman/Packman/game/Map/CGround.cpp
@@ -10,6 +10,7 @@
 void CGround::Create(float xz_size)
 {
 	m_box.Create(tkEngine::CVector3(xz_size*10, 1.0f, xz_size*10), 0x88888888, true);
+	m_pEffect = tkEngine::CEngine::Instance().EffectManager().LoadEffect("Assets/presetShader/ColorNormalPrim.fx");
 }
 void CGround::Start()
 {
@@ -24,11 +25,29 @@ void CGround::Update()
 void CGround::Render(tkEngine::CRenderContext& renderContext)
 {
 	CGameManager& gm = CGameManager::GetInstance();
-	m_box.RenderLight(
+	CMatrix mMVP = gm.GetGameCamera().GetViewProjectionMatrix();
+	const CMatrix& mWorld = m_box.GetWorldMatrix();
+	const CMatrix& mLVP = CEngine::Instance().ShadowMap().GetLVPMatrix();
+	mMVP.Mul(mWorld, mMVP);
+	CMatrix mRot = m_box.GetRotationMatrix();
+	m_pEffect->SetTechnique(renderContext, "ColorNormalShadowPrim");
+	m_pEffect->Begin(renderContext);
+	m_pEffect->BeginPass(renderContext, 0);
+	m_pEffect->SetValue(renderContext, "g_mWVP", &mMVP, sizeof(mMVP));
+	m_pEffect->SetValue(renderContext, "g_worldRotationMatrix", &mRot, sizeof(mRot));
+	m_pEffect->SetValue(renderContext, "g_mWorld", &mWorld, sizeof(mWorld));
+	m_pEffect->SetValue(renderContext, "g_mLVP", &mLVP, sizeof(mLVP));
+	m_pEffect->SetValue(
 		renderContext,
-		gm.GetGameCamera().GetViewProjectionMatrix(),
-		gm.GetGroundLight(),
-		false
-	);
+		"g_light",
+		&gm.GetGroundLight(),
+		sizeof(CLight)
+		);
+	m_pEffect->SetTexture(renderContext, "g_shadowMap", CEngine::Instance().ShadowMap().GetTexture());
+	m_pEffect->CommitChanges(renderContext);
+	m_box.Render(renderContext);
+
+	m_pEffect->EndPass(renderContext);
+	m_pEffect->End(renderContext);
 }
 	
--- Packman/Packman/game/Map/CGround.h
+++ Packman/Packman/game/Map/CGround.h
@@ -9,23 +9,12 @@
 
 class CGround : public tkEngine::IGameObject{
 public:
-	/*!
-	 *@brief	Updateが初めて呼ばれる直前に一度だけ呼ばれる処理。
-	 */
 	void Start() override final;
-	/*!
-	 *@brief	更新処理。60fpsなら16ミリ秒に一度。30fpsなら32ミリ秒に一度呼ばれる。
-	 */
 	void Update() override final;
-	/*!
-	 *@brief	描画処理。60fpsなら16ミリ秒に一度。30fpsなら32ミリ秒に一度呼ばれる。
-	 */
 	void Render(tkEngine::CRenderContext& renderContext) override final;
-	/*!
-	 *@brief	地面作成。
-	 */
 	void Create( float xz_size );
 private:
 	tkEngine::CBoxShape		m_box;	
+	tkEngine::CEffect*		m_pEffect;
 };
 #endif 
