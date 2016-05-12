811192bbb08c30dbaa746cf2218fa3cee60da8fc
KawaharaKiyohara <nezumimusume@gmail.com>
2016-05-12 19:15:24 +0900

    ファイル出力を修正。

--- tkTools/Assets/Editor/OutputCollitionInfo.cs
+++ tkTools/Assets/Editor/OutputCollitionInfo.cs
@@ -21,9 +21,9 @@ public class OutputCollitionInfo : EditorWindow
             }
             headerTxt += string.Format("//{0}\n", tr.name);
             headerTxt += "{\n";
-            headerTxt += string.Format("\tD3DXVECTOR3({0}f, {1}f, {2}f),             //平行移動\n", tr.position.x, tr.position.y, tr.position.z);
-            headerTxt += string.Format("\tD3DXQUATERNION({0}f, {1}f, {2}f, {3}f ),    //回転\n", tr.rotation.x, tr.rotation.y, tr.rotation.z, tr.rotation.w);
-            headerTxt += string.Format("\tD3DXVECTOR3({0}f, {1}f, {2}f ),            //拡大\n", tr.lossyScale.x, tr.lossyScale.y, tr.lossyScale.z);
+            headerTxt += string.Format("\tD3DXVECTOR3({0:f}f, {1:f}f, {2:f}f),             //平行移動\n", tr.position.x, tr.position.y, tr.position.z);
+            headerTxt += string.Format("\tD3DXQUATERNION({0:f}f, {1:f}f, {2:f}f, {3:f}f ),  //回転\n", tr.rotation.x, tr.rotation.y, tr.rotation.z, tr.rotation.w);
+            headerTxt += string.Format("\tD3DXVECTOR3({0:f}f, {1:f}f, {2:f}f ),            //拡大\n", tr.lossyScale.x, tr.lossyScale.y, tr.lossyScale.z);
             headerTxt += "},\n";
         }
       
