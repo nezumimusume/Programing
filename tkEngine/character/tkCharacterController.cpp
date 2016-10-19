1b3aab31ea830f56e07683c76908ddbbad3f489b
KawaharaKiyohara <nezumimusume@gmail.com>
2016-10-19 18:03:48 +0900

    衝突解決を改良。

--- tkEngine/character/tkCharacterController.cpp
+++ tkEngine/character/tkCharacterController.cpp
@@ -146,7 +146,7 @@ namespace tkEngine{
 				CVector3 newPos;
 				SweepResultWall callback;
 				callback.me = rigidBody.GetBody();
-				callback.startPos = position;
+				callback.startPos = posTmp;
 				CVector3 addPosXZ = addPos;
 				addPosXZ.y = 0.0f;
 				if (addPosXZ.Length() > 0.0001f) {
@@ -158,25 +158,25 @@ namespace tkEngine{
 				if (callback.isHit) {
 					//当たった。
 					//壁。
-					nextPosition.x = callback.hitPos.x;
-					nextPosition.z = callback.hitPos.z;
-					//半径分押し戻す。
+					CVector3 vT0, vT1;
+					//XZ平面上での移動後の座標をvT0に、交点の座標をvT1に設定する。
+					vT0.Set(nextPosition.x, 0.0f, nextPosition.z);
+					vT1.Set(callback.hitPos.x, 0.0f, callback.hitPos.z);
+					//めり込みが発生している移動ベクトルを求める。
+					CVector3 vMerikomi;
+					vMerikomi.Subtract(vT0, vT1);
+					//XZ平面での衝突した壁の法線を求める。。
 					CVector3 hitNormalXZ = callback.hitNormal;
 					hitNormalXZ.y = 0.0f;
 					hitNormalXZ.Normalize();
-					CVector3 t = hitNormalXZ;
-					t.Scale(radius);
-					nextPosition.Add(t);
-					//続いて壁に沿って滑らせる。
-					t.Cross(hitNormalXZ, CVector3::Up);
-					t.Normalize();
-					//押し戻しで動いた分は減算する。
-					CVector3 t2;
-					t2.Subtract(nextPosition, position);
-					t2.y = 0.0f;
-					addPosXZ.Subtract(t2);
-					t.Scale(t.Dot(addPosXZ));
-					nextPosition.Add(t);
+					//めり込みベクトルを壁の法線に射影する。
+					float fT0 = hitNormalXZ.Dot(vMerikomi);
+					//押し戻し返すベクトルを求める。
+					//押し返すベクトルは壁の法線に射影されためり込みベクトル+半径。
+					CVector3 vOffset;
+					vOffset = hitNormalXZ;
+					vOffset.Scale(-fT0 + radius);
+					nextPosition.Add(vOffset);
 				}
 				else {
 					//どことも当たらないので終わり。
@@ -184,6 +184,7 @@ namespace tkEngine{
 				}
 				loopCount++;
 				if (loopCount == 5) {
+					//移動できない。
 					break;
 				}
 			}
@@ -238,46 +239,6 @@ namespace tkEngine{
 				nextPosition.y = callback.hitPos.y + offset - radius;
 			}
 		}
-		//下方向を調べる。
-		/*{
-			
-
-			btTransform start, end;
-			start.setIdentity();
-			end.setIdentity();
-			start.setOrigin(btVector3(nextPosition.x, nextPosition.y + height * 0.5f + radius + 0.1f, nextPosition.z));
-			CVector3 newPos;
-			SweepResultGround callback;
-			callback.me = rigidBody.GetBody();
-			callback.startPos = (*(CVector3*)&start.getOrigin());
-			newPos = (*(CVector3*)&start.getOrigin());
-			newPos.y += fabsf(nextPosition.y - position.y) * 0.5f;
-
-			end.setOrigin(btVector3(newPos.x, newPos.y, newPos.z));
-
-			PhysicsWorld().ConvexSweepTest((const btConvexShape*)collider.GetBody(), start, end, callback);
-
-			if (callback.isHit) {
-				//当たった。
-				CVector3 Circle;
-				float x = 0.0f;
-				float offset = 0.0f;	//押し戻す量。
-				Circle = CVector3::Zero;
-
-				Circle = nextPosition;
-				Circle.y = callback.hitPos.y;//円の中心
-				CVector3 v;
-				v.Subtract(Circle, callback.hitPos);
-				x = v.Length();//物体の角とプレイヤーの間の横幅の距離が求まる。
-
-				offset = sqrt(max(0.0f, radius*radius - x*x));//yの平方根を求める。
-
-				moveSpeed.y = 0.0f;
-				isJump = false;
-				nextPosition.y = callback.hitPos.y + offset - radius ;
-			}
-		}*/
-
 		position = nextPosition;
 		btRigidBody* btBody = rigidBody.GetBody();
 		//剛体を動かす。
