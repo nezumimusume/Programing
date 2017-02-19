64a88cb366c6e09ab8d1c264d35316eb42663e6d
TakayukiKiyohara <nezumimusume30@gmail.com>
2017-02-20 00:23:47 +0900

    いろいろファイルを追加

--- tkTools/Assets/Editor/OutputLocationInfo.cs
+++ tkTools/Assets/Editor/OutputLocationInfo.cs
@@ -4,7 +4,45 @@ using UnityEditor;
 using System.IO;
 using System.Text;
 
-public class OutputLocationInfo : MonoBehaviour {
+public class tkTools : EditorWindow
+{
+    GameObject selectLocObject = null;
+    GameObject locationObject = null;
+    [MenuItem("Window/tkTools")]
+    static void Open()
+    {
+        GetWindow<tkTools>();
+    }
+    void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField("配置するオブジェクトを選択");
+        selectLocObject = EditorGUILayout.ObjectField(selectLocObject, typeof(GameObject), true) as GameObject;
+        if (GUILayout.Button("シーンに追加"))
+        {
+            //ボタンを押された。
+            if (selectLocObject != null)
+            {
+                //配置する。
+                //
+                locationObject = GameObject.Find("Location");
+                if(locationObject == null)
+                {
+                    locationObject = new GameObject("Location");
+                }
+                
+                GameObject addObj = UnityEditor.PrefabUtility.InstantiatePrefab(selectLocObject) as GameObject;
+                
+                Vector3 scale = addObj.transform.localScale;
+                scale.x *= -1.0f;
+                addObj.transform.localScale = scale;
+                addObj.transform.parent = locationObject.transform;
+                MapChip chip = addObj.AddComponent<MapChip>();
+                chip.invRot = Quaternion.Inverse(addObj.transform.localRotation);
+            }
+        }
+        EditorGUILayout.EndVertical();
+    }
     /// <summary>
     /// 名前から不要な文字をトリム
     /// </summary>
@@ -35,7 +73,8 @@ public class OutputLocationInfo : MonoBehaviour {
         //マップチップを出力。
         foreach (Transform tr in locs)
         {
-            if (!tr.GetComponent<MapChip>())
+            MapChip mapChip = tr.GetComponent<MapChip>();
+            if (!mapChip)
             {
                 //マップチップが取得できないオブジェクトは無視。
                 continue;
@@ -44,8 +83,9 @@ public class OutputLocationInfo : MonoBehaviour {
             outputTxt += string.Format("//{0}\n", modelName);
             outputTxt += "{\n";
             outputTxt += string.Format("\t\"{0}\",\n", modelName);
-            outputTxt += string.Format("\tCVector3({0:f}f, {1:f}f, {2:f}f),             //平行移動\n", tr.localPosition.x, tr.localPosition.y, tr.localPosition.z);
-            outputTxt += string.Format("\tCQuaternion({0:f}f, {1:f}f, {2:f}f, {3:f}f ),  //回転\n", tr.localRotation.x, tr.localRotation.y, tr.localRotation.z, tr.localRotation.w);
+            outputTxt += string.Format("\tCVector3({0:f}f, {1:f}f, {2:f}f),             //平行移動\n", tr.position.x, tr.position.y, tr.position.z);
+            Quaternion rot = tr.localRotation * mapChip.invRot;
+            outputTxt += string.Format("\tCQuaternion({0:f}f, {1:f}f, {2:f}f, {3:f}f ),  //回転\n", rot.x, rot.y, rot.z, rot.w);
             outputTxt += "},\n";
         }
         StreamWriter sw = new StreamWriter(@"C:\GitHub\Programing\UnityChanDemo\SkinAnimationDemo\locationInfo.h", false, Encoding.UTF8);
@@ -73,11 +113,6 @@ public class OutputLocationInfo : MonoBehaviour {
         sw = new StreamWriter(@"C:\GitHub\Programing\UnityChanDemo\SkinAnimationDemo\Enemy\EnemyInfo.h", false, Encoding.UTF8);
         sw.Write(outputTxt);
         sw.Close();
-
-    }
-
-    void OnGUI()
-    {
 
     }
 }
--- tkTools/Assets/scripts/MapChip.cs
+++ tkTools/Assets/scripts/MapChip.cs
@@ -2,9 +2,14 @@
 using System.Collections;
 
 public class MapChip : MonoBehaviour {
+    public Quaternion invRot { get; set; }
 	// Use this for initialization
+    void Awake()
+    {
+        invRot = Quaternion.identity;
+    }
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
