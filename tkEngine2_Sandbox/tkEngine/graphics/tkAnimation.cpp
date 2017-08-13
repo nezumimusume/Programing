f22f8e5eccadfd9ed67bd9d037dfd12866a2850e
TakayukiKiyohara <1>
2017-08-13 12:18:40 +0900

    CAnimationを追加。

---
+++ tkEngine2_Sandbox/tkEngine/graphics/tkAnimation.cpp
@@ -0,0 +1,75 @@
+/*!
+ * @brief	アニメータークラス。
+ */
+
+
+#include "tkEngine/tkEnginePrecompile.h"
+#include "tkEngine/graphics/tkAnimation.h"
+
+namespace tkEngine{
+	CAnimation::CAnimation()
+	{
+	}
+	CAnimation::~CAnimation()
+	{
+	}
+	void CAnimation::Init(CSkinModelData& skinModelData)
+	{
+		std::vector<CAnimationClipPtr>& animClips = skinModelData.GetAnimationClips();
+		for(auto& clip : animClips){
+			m_animationClips.push_back(clip.get());
+		}
+		m_skeleton = &skinModelData.GetSkeleton();
+	}
+	/*!
+	* @brief	アニメーションを進める。
+	*@param[in]	deltaTime		アニメーションを進める時間(単位：秒)。
+	*/
+	void CAnimation::Update(float deltaTime)
+	{
+		if (m_currentAnimationClip == nullptr 
+			|| m_skeleton == nullptr 
+		) {
+			return;
+		}
+		//とりあえず適当に進めていく。
+		int numBones = m_skeleton->GetNumBones();
+		const auto& keyFramePtrListArray = m_currentAnimationClip->GetKeyFramePtrListArray();
+		const auto& topBoneKeyFrameList = keyFramePtrListArray.at(0);
+		m_globalTime += deltaTime;
+		while (true) {
+			if (m_currentKeyframeNo >= topBoneKeyFrameList.size()) {
+				//終端まで行った。
+				if (m_currentAnimationClip->IsLoop()) {
+					//ループ。
+					m_currentKeyframeNo = 0;
+					m_globalTime = 0.0f;
+				}
+				else {
+					//ワンショット再生。
+					m_currentKeyframeNo--;
+				}
+				break;
+			}
+			if (topBoneKeyFrameList.at(m_currentKeyframeNo)->time >= m_globalTime) {
+				//終わり。
+				break;
+			}
+			//次へ。
+			m_currentKeyframeNo++;
+		}
+		//スケルトンに反映させていく。
+		//とりあえずテストデータはフルキーなので何も考えなくてよさげ。
+		for (const auto& keyFrameList : keyFramePtrListArray) {
+			if (keyFrameList.size() == 0) {
+				break;
+			}
+			//現在再生中のキーフレームを取ってくる。
+			Keyframe* keyframe = keyFrameList.at(m_currentKeyframeNo);
+			m_skeleton->SetBoneLocalMatrix(
+				keyframe->boneIndex, 
+				keyframe->transform
+			);
+		}
+	}
+}
---
+++ tkEngine2_Sandbox/tkEngine/graphics/tkAnimation.h
@@ -0,0 +1,78 @@
+/*!
+ * @brief	アニメーション
+ */
+
+#pragma once
+
+#include "tkEngine/graphics/tkAnimationClip.h"
+
+namespace tkEngine{
+	
+	class CSkeleton;
+	/*!
+	 * @brief	アニメーションクラス。
+	 */
+	class CAnimation{
+	public:
+		CAnimation();
+		~CAnimation();
+		/*!
+		 * @brief	初期化。
+		 *@param[in]	skinModelData	スキンモデルデータ。
+		 */
+		void Init(CSkinModelData& skinModelData);
+		/*!
+		* @brief	アニメーションを進める。
+		*@param[in]	deltaTime		アニメーションを進める時間(単位：秒)。
+		*/
+		void Update(float deltaTime);
+		/*!
+		 *@brief	アニメーションの再生。
+		 *@param[in]	clipName	アニメーションクリップの名前。
+		 */
+		void Play(const wchar_t* clipName)
+		{
+			if (clipName == nullptr) {
+				TK_WARNING("clipName is NULL");
+				return;
+			}
+			auto it = std::find_if(
+				m_animationClips.begin(),
+				m_animationClips.end(), 
+				[clipName](auto& clip) {return clip->GetName() == clipName; }
+			);
+			if (it == m_animationClips.end()) {
+				TK_WARNING("not found clip");
+				return;
+			}
+			m_currentAnimationClip = *it;
+			m_globalTime = 0.0f;
+			m_currentKeyframeNo = 0;
+		}
+		/*!
+		*@brief	アニメーションクリップのループフラグを設定します。
+		*@param[in]	clipName	アニメーションクリップの名前。
+		*@param[in]	flag		フラグ。
+		*/
+		void SetAnimationClipLoopFlag(const wchar_t* clipName, bool flag)
+		{
+			auto it = std::find_if(
+				m_animationClips.begin(),
+				m_animationClips.end(),
+				[clipName](auto& clip) {return clip->GetName() == clipName; }
+			);
+			if (it == m_animationClips.end()) {
+				//見つからなかった。
+				return ;
+			}
+			(*it)->SetLoopFlag(flag);
+		}\
+	private:
+		std::vector<CAnimationClip*>	m_animationClips;	//!<アニメーションクリップの配列。
+		CSkeleton* m_skeleton = nullptr;	//!<アニメーションを適用するスケルトン。
+		CAnimationClip* m_currentAnimationClip = nullptr;	//!<現在再生中のアニメーションクリップ。
+		float m_globalTime = 0.0f;
+		int m_currentKeyframeNo = 0;		//!<現在再生中のアニメーションキーフレーム番号。
+		
+	};
+}
