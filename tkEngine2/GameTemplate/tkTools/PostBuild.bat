c2bfa546d6bd5dc456e847bb446df4d08e6d4780
TakayukiKiyohara <1>
2017-10-17 11:58:24 +0900

    カスタムビルドステップを追加。

--- tkEngine2/GameTemplate/tkTools/PostBuild.bat
+++ tkEngine2/GameTemplate/tkTools/PostBuild.bat
@@ -3,7 +3,4 @@ pushd ..\\Game\\
 rem ddsファイルをResourceの下にコピー。
 xcopy /Y /D *.dds .\Resource\*
 
-rem AssetsフォルダからResourceフォルダにごそっとコピー。
-rem 画像ファイルとfbxはコピーされないよ！！！
-xcopy /Y /D /I /S .\Assets .\Resource /EXCLUDE:..\tkTools\exlcude.txt
 popd
