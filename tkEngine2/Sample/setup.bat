db3e2a5976505943a3a42f5845e209e33aec2202
TakayukiKiyohara <nezumimusume30@gmail.com>
2018-02-19 00:02:48 +0900

    Effekseer追加。

--- tkEngine2/Sample/setup.bat
+++ tkEngine2/Sample/setup.bat
@@ -1,9 +1,11 @@
 @rem サンプルの各フォルダにtkEngineへのシンボリックリンクを作成する。
 @setlocal
 @set CURRENT_DIR=%~dp0
-@pushd "%CURRENT_DIR%"
+@pushd "%CURRENT_DIR%
+
 @for /d %%a in (.\*) do (call :makeSymbolicLink "%%a")
 @goto end
+
 
 :makeSymbolicLink
 @set SAMPLE_FOLDER=%~1
